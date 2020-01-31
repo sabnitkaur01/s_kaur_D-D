456fe1afb4cb76b0010bd545a675252bf33f0099
sabnit kaur <sabnitkaur01@sabnits-MBP.fc.ca>
2020-01-31 13:32:02 -0500

    modified

--- script/app.js
+++ script/app.js
@@ -1,12 +1,21 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+				puzzlePieces = document.querySelectorAll('.puzzle-pieces img')
+       	gameBoard = document.querySelector('.puzzle-board');
+
+	const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 	function changeImageSet() {
-		// change all the image elements on the page -> draggable image sources,
-		// and set the drop zone background
+		// change all the image elements on the page -> draggable image sources
+		// change the image elements on the left to match the selected puzzle
+		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`);
 
-		debugger;
+
+
+		// and set the drop zone background image based on the puzzle the user selects
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
+		//debugger;
 	}
 
 
@@ -15,6 +24,10 @@
 
 	// click on the button buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet))
+
+	//call the function and pass in the first nav button as a reference
+	// research call, apply and bind -> look at MDN
+	changeImageSet.call(puzzleButtons[0]);
 
 
 
