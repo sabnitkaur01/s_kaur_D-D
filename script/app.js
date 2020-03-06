71a272fab586ba800040282afda3720637b01c5d
sabnit kaur <sabnitkaur01@sabnits-MBP.fc.ca>
2020-03-06 15:20:35 -0500

    almost done

--- script/app.js
+++ script/app.js
@@ -43,7 +43,7 @@
 				console.log('dropped on a drop zone');
 
 			let currentImage = event.dataTransfer.getData("text/plain");
-			// add that image to drop zone where we adding our image 
+			// add that image to drop zone where we adding our image on
 			event.target.appendChild(document.querySelector(`#${currentImage}`));
 
 
